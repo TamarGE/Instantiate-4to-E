6e39b4b88e1ed5dd2a924f95d0c3690163693bbe
TamarGE <emiliaguerson@gmail.com>
2022-05-10 12:07:28 -0300

    Destroy
    Con Destroy, programamos para que se destryan los clones después de un segundo

--- Assets/Scripts/Instantiator.cs
+++ Assets/Scripts/Instantiator.cs
@@ -7,13 +7,17 @@ public class Instantiator : MonoBehaviour
 {
     public GameObject objectToClone;
     public Text cloneAmount;
+    GameObject clon;
 
     public void MultiInstantiate()
     {
         
         for (int counter = 0; counter < int.Parse(cloneAmount.text); counter++)
         {
-            Instantiate(objectToClone);
+            clon = Instantiate(objectToClone);
+            Destroy(clon, 1); //destruír el clon después de un seg
         }
+        
+
     }
 }
